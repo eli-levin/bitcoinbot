c9d13eb5a00b7712f6153d22bf53548843e178cb
Eli Levin <Eli Levin>
2017-01-08 01:47:51 -0600

    poop

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const express = require('express'),
-	bodyParser = require('body-parser'),
-	request = require('request');
+    bodyParser = require('body-parser'),
+    request = require('request');
 
 const app = express();
 
@@ -12,24 +12,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-	res.send('bitcoin messages go here...');
+    res.send('bitcoin messages go here...');
 });
 
 app.get('/webhook', (req, res) => {
-	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-		console.log("Validating webhook");
-		res.status(200).send(req.query['hub.challenge']);
-	}
-	else{
-		console.error("Failed validation. Make sure the validation tokens match.");
-		res.sendStatus(403);          
-	}  
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'btcfbbot93d17fe1') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    }
+    else{
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);          
+    }  
 });
 
 app.post('/webhook', (req, res) => {
-	let token = process.env.FB_PAGE_ACCESS_TOKEN
+    let token = process.env.FB_PAGE_ACCESS_TOKEN;
 });
 
 app.listen(app.get('port'), () => {
-	console.log(`listening at http:\/\/localhost:${app.get('port')}`);
+    console.log(`listening at http:\/\/localhost:${app.get('port')}`);
 });
