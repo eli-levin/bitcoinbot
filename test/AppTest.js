c0271f2ffcffa9b8c4ff6bf3d999352f04b9d556
Eli Levin <eli.j.wasserman@gmail.com>
2017-02-23 19:06:31 -0600

    adding handler file and new test files

---
+++ test/AppTest.js
@@ -0,0 +1,14 @@
+// Integration Tests
+// 2017 (c) Eli Levin
+// BitcoinBot
+/////////////////////////
+
+'use strict'
+
+const assert        = require('assert'),
+      btcbot        = require('../app.js'),
+      os            = require('os');
+
+//
+// Test app
+//
--- test/UnitTests.js
+++ test/UnitTests.js
@@ -1,16 +1,16 @@
 /////////////////////////
-// test.js
+// Unit Tests
 // 2017 (c) Eli Levin
 // BitcoinBot
 /////////////////////////
 
 'use strict'
 
-const assert        = require('assert'),
-      //btcbot        = require('../app.js'),
-      BitcoinGuru   = require('../lib/BitcoinGuru.js'),
-      FacebookGraph = require('../lib/FacebookGraph.js'),
-      os            = require('os');
+const assert         = require('assert'),
+      //MessageHandler = require('../lib/MessageHandler.js'),
+      BitcoinGuru    = require('../lib/BitcoinGuru.js'),
+      FacebookGraph  = require('../lib/FacebookGraph.js'),
+      os             = require('os');
 
 //
 // Stuff
@@ -19,24 +19,18 @@ let fb   = new FacebookGraph();
 let guru = new BitcoinGuru();
 
 //
-// Test app
+// Test MessageHandler
 //
-// describe('app', () => {
-//     describe('#getUserProfile()', () => {
-//         it('Should fetch the test user\'s fb profile info from fb API.', (done) => {
-//             btcbot.getUserProfile(process.env.TEST_USER_ID, (err, res, body) => {
-//                 let userProfile = JSON.parse(body);
-//                 assert.equal(res.statusCode, 200, 'Expected successful status code in response ' + JSON.stringify(res, null, '\t'));
-//                 assert(!err, 'Expected no error in response ' + JSON.stringify(err, null, '\t'));
-//                 assert(userProfile, 'Expected body object. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("first_name"), 'Expected body object with property first_name. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("last_name"), 'Expected body object with property last_name. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("profile_pic"), 'Expected body object with property profile_pic. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("locale"), 'Expected body object with property locale. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("timezone"), 'Expected body object with property timezone. Body = ' + JSON.stringify(res, null, '\t'));
-//                 assert(userProfile.hasOwnProperty("gender"), 'Expected body object with property gender. Body = ' + JSON.stringify(res, null, '\t'));
-//                 done();
-//             });
+// describe('MessageHandler', () => {
+//     describe('#formulateInsightMessage()', () => {
+//         it('Should create an insight message with a crude analysis of current trends.', (done) => {
+//             // todo: implement tests
+//         });
+//     });
+
+//     describe('#onReceievedMessage()', () => {
+//         it('Should ...'/*todo: make this function not shit*/, (done) => {
+//             // todo: implement tests
 //         });
 //     });
 // });
