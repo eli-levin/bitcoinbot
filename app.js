168fcc4088c6f9b2376b7b7c8faa3de8bc8c38e6
Eli Levin <eli.j.wasserman@gmail.com>
2017-02-26 22:38:28 -0600

    removing undeeded includes and macros

--- app.js
+++ app.js
@@ -9,26 +9,9 @@
 const express        = require('express'),
       request        = require('request'),
       bodyParser     = require('body-parser'),
-      BitcoinGuru    = require('./lib/BitcoinGuru.js'),
-      FacebookGraph  = require('./lib/FacebookGraph.js'),
       MessageHandler = require('./lib/MessageHandler.js'),
       os             = require('os'),
       app            = express();
-
-//
-// Macros
-//
-const CB_CREDS              = 'public',
-      FB_SEND_URI           = 'https://graph.facebook.com/v2.8/me/messages',
-      FB_GENERIC_API_URI    = 'https://graph.facebook.com/v2.8/',
-      ERROR_RESPONSE_STR    = 'Oops, something went wrong. Try again later...',
-      DEFAULT_RESPONSE_STR  = 'Sorry, I don\'t know what that means. Say "help" for more info!',
-      USER_PROFILE_OPTIONS  = 'first_name,last_name,profile_pic,locale,timezone,gender',
-      INSIGHT_LEGAL_WARNING = '(Do not take this as investment advice. Consult your own financial advisor for personal investment counsel.)',
-      HELP_RESPONSE_STR     = 'Here is a list of things I\'ll respond to:' + os.EOL +
-                              'help --> gets list of commands' + os.EOL +
-                              'price --> tells you spot price of BTC to your local currencey (USD if not specified).' + os.EOL +
-                              'insight --> tells you current buyer trends';
 
 //
 // Init port in app table and process the url and json parsers
